254336cb0cb97f25aaf69f73e7d71f9b326006a2
MixelMeza <mixolms@gmail.com>
2024-10-20 12:49:25 -0500

    v3

--- src/main/java/com/example/syscaso/repository/AccesoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.syscaso.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.syscaso.entity.Acceso;
-@Repository
-public interface AccesoRepository extends JpaRepository<Acceso, Long>{
-
-}
